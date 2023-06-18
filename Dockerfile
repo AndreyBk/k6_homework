02bd324f92950be44af7d4b429a39d20c65425a8
Andrey <propre@mail.ru>
2023-06-18 11:35:21 +0300

    init homework

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM grafana/k6:0.38.2
-COPY sample.js .
-ENTRYPOINT ["k6", "run", "sample.js"]
--- otus.js
+++
@@ -1,9 +0,0 @@
-import http from 'k6/http';
-
-export default function(){
-http.get("https://test.k6.io/contacts.php");
-
-console.log('hello!!!')
-
-http.post('http://test.k6.io/flip_coin.php')
-};
